5cc5818ffba94d0a5836d89c5547e605e36f5615
AKP2611 <ashishpatelk2611@gmail.com>
2017-11-08 10:17:22 +0530

    Updated permission scenario...
    Changes according to ShowRequestPermissionRationale

--- mylibrary/src/main/java/com/abc/mylibrary/PermissionUtils.java
+++ mylibrary/src/main/java/com/abc/mylibrary/PermissionUtils.java
@@ -24,39 +24,72 @@ import android.support.v4.content.ContextCompat;
 import java.util.ArrayList;
 
 public class PermissionUtils {
-    public static boolean requestPermission(
-            Activity activity, int requestCode, String... permissions) {
+     private static final String TAG = "PermissionUtils";
+
+    public static boolean requestPermission(Activity activity, int requestCode, String... permissions) {
+
         boolean granted = true;
         ArrayList<String> permissionsNeeded = new ArrayList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
-        for (String s : permissions) {
-            int permissionCheck = ContextCompat.checkSelfPermission(activity, s);
-            boolean hasPermission = (permissionCheck == PackageManager.PERMISSION_GRANTED);
-            granted &= hasPermission;
-            if (!hasPermission) {
-                permissionsNeeded.add(s);
+
+            for (String s : permissions) {
+                int permissionCheck = ContextCompat.checkSelfPermission(activity, s);
+                boolean hasPermission = (permissionCheck == PackageManager.PERMISSION_GRANTED);
+                granted &= hasPermission;
+                if (!hasPermission) {
+                    permissionsNeeded.add(s);
+                }
             }
+
+
+            if (granted) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(activity,
+                        permissionsNeeded.toArray(new String[permissionsNeeded.size()]),
+                        requestCode);
+                return false;
+
+            }
+        } else {
+            return true;
+            // do something for phones running an SDK before M
         }
 
-        if (granted) {
-            return true;
-        } else {
-            ActivityCompat.requestPermissions(activity,
-                    permissionsNeeded.toArray(new String[permissionsNeeded.size()]),
-                    requestCode);
-            return false;
-        }
+
     }
 
 
-    public static boolean permissionGranted(int requestCode, int permissionCode, int[] grantResults) {
+    public static boolean permissionGranted(Activity activity, int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int permissionCode) {
         if (requestCode == permissionCode) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                return true;
-            } else {
-                return false;
+
+            for (String permission : permissions) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                    //denied
+                    Log.w("denied", permission);
+
+                    Toast.makeText(activity, "Required permission : " + permission + "", Toast.LENGTH_SHORT).show();
+                    return false;
+                } else {
+                    if (ActivityCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED) {
+                        //allowed
+                        Log.w("allowed", permission);
+
+                        return true;
+                    } else {
+                        //set to never ask again
+                        Log.w("set to never ask again", permission);
+                        //do something here.
+                        Toast.makeText(activity, "Enable permission in Settings :" + permission + "", Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                }
             }
+
         }
         return false;
     }
+
+
 }
